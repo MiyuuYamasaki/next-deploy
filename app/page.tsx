da1a0cda7107ca19e9e014813674ebdf15a17957
Yamasaki Miyuu <y-miyu-2429@occ.co.jp>
2024-06-07 15:20:34 +0900

    1st commit

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,8 @@ export default function Home() {
         />
       </div>
 
+      <h1>Hello World 20240607</h1>
+
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
