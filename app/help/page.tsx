1519676e00ee97759e8021b6c5960cefc3837413
Yamasaki Miyuu <y-miyu-2429@occ.co.jp>
2024-06-10 11:26:06 +0900

    create help.about

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="coontainer mx auto">
       <div className="flex flex-col h-screen items-center justify-center">
-        <h1 className="font-bold text-7xl text-blue-900">Home Page</h1>
-        <Link href="/help">help</Link>
-        <Link href="/about">about</Link>
+        <h1 className="font-bold text-5xl text-yellow-700">Help Page</h1>
+        <Link href="/">Home</Link>
       </div>
     </div>
   );
